d821c4c39afbe9ddf67d1f13d82df80eaca6cd80
Volkan K <7557299+kumbasar@users.noreply.github.com>
2024-06-12 16:21:57 +0300

    decrease retry time

--- src/constants.py
+++ src/constants.py
@@ -3,8 +3,8 @@ REGEX_CONTINUE = "//a[contains(text(),'Continuar')]"
 # Time between steps (interactions with forms): 0.5 seconds
 STEP_TIME = 0.5
 
-# Wait time between retries/checks for available dates: 10 minutes
-RETRY_TIME = 10 * 60
+# Wait time between retries/checks for available dates: 3 minutes
+RETRY_TIME = 3 * 60
 
 # Wait time when an exception occurs: 30 minutes
 EXCEPTION_TIME = 30 * 60
