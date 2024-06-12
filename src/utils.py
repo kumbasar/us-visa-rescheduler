4b99175707db51a912a562ab7b29de673dac16a2
Volkan K <7557299+kumbasar@users.noreply.github.com>
2024-06-12 16:21:21 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 import configparser
 
 from prettytable import PrettyTable
-from selenium import webdriver
+from seleniumwire import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -29,8 +29,11 @@ def load_config(config_file):
 # This means that the new date should be in November and the day should be greater than or equal to 5
 
 # No custom condition wanted for the new scheduled date
+
+
 def my_condition(month, day):
     return None
+
 
 def get_driver(local_use, hub_address):
     """
